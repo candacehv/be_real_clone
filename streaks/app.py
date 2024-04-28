b4ebc3bd0ab5bbb7941ee70863fde21765fcbfa7
candacehv <candace.volke@gmail.com>
2024-04-28 15:51:47 -0400

    update all

--- streaks/app.py
+++ streaks/app.py
@@ -11,6 +11,7 @@ def app(event, context):
     for event in event['Records']:
         body = json.loads(event['body'])
         # print(body['Message'])
+        print("Received event: " + str(event))
         message = json.loads(body['Message'])
 
         try:
@@ -19,14 +20,30 @@ def app(event, context):
             released_timestamp_int = message['released_timestamp']
             upload_timestamp_int = message['upload_timestamp']
             is_late = upload_timestamp_int - released_timestamp_int > 120
+
+            # convert to days
+            released_day = released_timestamp_int // 86400
+            upload_day = upload_timestamp_int // 86400
+            # print(f'release days {released_day}')
+            last_post_info = get_last_post_info(user_id, released_timestamp_int)
+            # print(f'last_post_info  {last_post_info}')
+            last_post_day = last_post_info.get('last_post_day', released_day)
+            current_streak = last_post_info.get('streak', 0)
+
+            if released_day - last_post_day == 1:
+                current_streak += 1
+            else:
+                current_streak = 1 # Reset streak if more than a day has passed
+
             res = streaks.put_item(
                 Item={'upload_timestamp_int': upload_timestamp_int, 
                       'user_id': user_id, 
                       'is_late': is_late, 
                       'released_timestamp_int': released_timestamp_int, 
-                      'load_dt': now
+                      'load_dt': now,
+                      'streak': current_streak
                       }) 
-            print(f'Streaks table load successful: {res}')
+            # print(f'Streaks table load successful: {res}')
             return {
                 'statusCode': 200,
                 'body': json.dumps({'message': 'Load successful'})
@@ -37,8 +54,34 @@ def app(event, context):
                 'statusCode': 500,
                 'body': json.dumps({'message': 'Load failed'})
             }
+        
             
         sqs.delete_messages(
             queue_url='https://sqs.us-east-1.amazonaws.com/851725573367/PostStreaksQueue', 
             ReceiptHandle= body['ReceiptHandle'])
-        print(f'Message {body["MessageId"]} deleted from queue')
+        # print(f'Message {body["MessageId"]} deleted from queue')
+
+def get_last_post_info(user_id, current_released_timestamp_int):
+    streaks = db.Table('post_streaks')
+
+    response = streaks.query(
+        KeyConditionExpression='user_id = :user_id AND released_timestamp_int < :current_ts',
+        ExpressionAttributeValues={
+            ':user_id': user_id,
+            ':current_ts': current_released_timestamp_int
+        },
+        ScanIndexForward=False,  
+        Limit=1  
+    )
+
+    # print(f'last record {response}')
+    
+    items = response.get('Items', [])
+    if items:
+        last_post = items[0]
+        last_post_day = last_post['released_timestamp_int'] // 86400
+        current_streak = last_post.get('streak', 0)
+        return {'last_post_day': last_post_day, 'streak': current_streak}
+    else:
+        # Return default values if no previous post exists
+        return {'last_post_day': 0, 'streak': 0}
